31787109fb48d7357a048ac7ff9286f7747630ae
leonardogcavalcanti <leonardo.gcavalcanti@gmail.com>
2022-11-06 21:42:24 -0300

    :tada:

---
+++ main.js
@@ -0,0 +1,37 @@
+function creatGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
+  `
+}
+
+let delay = -0.4;
+function creatCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+    <div class="card" style="animation-delay:${delay}s">
+      <h2>${date}<span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  creatCard("24/11", "Quinta", creatGame("brazil", "16:00", 
+  "serbia")) +
+
+  creatCard(
+    "28/11",
+    "Segunda",
+    creatGame("brazil", "13:00", "switzerland") +          
+      creatGame("portugal", "16:00", "uruguai" )
+    
+    ) +
+
+  creatCard("02/12", "Sexta", creatGame("brazil", "16:00", 
+  "cameroo"))
